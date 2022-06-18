813eb08809f5773d22abd6c03782a4a71419f3ae
TheCreator2022 <106817598+TheCreator2022@users.noreply.github.com>
2022-06-18 10:26:08 -0300

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,5 +1,5 @@
 let modInfo = {
-	name: "The ??? Tree",
+	name: "The Train Tree",
 	id: "mymod",
 	author: "nobody",
 	pointsName: "points",
@@ -14,7 +14,7 @@ let modInfo = {
 // Set your version in num and name
 let VERSION = {
 	num: "0.0",
-	name: "Literally nothing",
+	name: "Exponential Idle?",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
